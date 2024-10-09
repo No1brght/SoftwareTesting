7e290175051aaa4b54c9e292e1a9504a07007870
Nagy Huba <NagyHuba@pg.local>
2024-10-09 09:37:12 +0200

    .\test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -2,11 +2,11 @@ import pytest
 from calculator import add, subtract, multiply, divide
 
 def test_add():
-    assert add (1,2) == 3
-    assert add (5,5) == 10
-    assert add (-1,1) == 0
-    assert add (-1,-1) == -2
-    assert add (0,0) == 0
+    assert add (1, 2) == 3
+    assert add (5, 5) == 10
+    assert add (-1, 1) == 0
+    assert add (-1, -1) == -2
+    assert add (0, 0) == 0
 
 def test_substract():
     assert subtract (10, 5) == 5
@@ -21,3 +21,10 @@ def test_multiply():
     assert multiply (-6, -12) == 72
     assert multiply (10, -13) == -130
     assert multiply (5, 5) == 25
+
+def test_divide():
+    assert divide (10, 5) == 2
+    assert divide (120, 10) == 12
+    assert divide (-6, -6) == 1
+    assert divide (10, -10) == -1
+    assert divide (5, 10) == 0.5
